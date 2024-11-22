1f2d9367291a400d20f05077a4208993a3c5e557
kaflovskyi-roman <kiraqeen2004@gmail.com>
2024-11-22 16:36:22 +0200

    [fix] changed Client and ClientGenerator

--- server/src/main/java/generators/ClientGenerator.java
+++ server/src/main/java/generators/ClientGenerator.java
@@ -2,57 +2,54 @@ package generators;
 
 import lombok.Setter;
 import models.Client;
+import models.Position;
 import strategy.tickets.FixedTicketsStrategy;
 import strategy.tickets.TicketsGenerationStrategy;
 import strategy.time.FixedTimeStrategy;
 import strategy.time.TimeGenerationStrategy;
 
+import java.util.Random;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+// delay поставив в секундах, подумайте, чи норм, чи забрати і в мілісекундах
 @Setter
 public class ClientGenerator {
     private TimeGenerationStrategy timeGenerationStrategy;
     private PrivilegeGenerator privilegeGenerator;
     private TicketsGenerationStrategy ticketsGenerationStrategy;
-    private Thread currentThread;
-    private volatile boolean running = true;
+    private ScheduledThreadPoolExecutor scheduler;
 
     public ClientGenerator(TimeGenerationStrategy timeGen, PrivilegeGenerator privilegeGen, TicketsGenerationStrategy ticketsGen) {
         this.timeGenerationStrategy = timeGen;
         this.privilegeGenerator = privilegeGen;
         this.ticketsGenerationStrategy = ticketsGen;
-
-        currentThread = new Thread(this::generateClients);
     }
 
-    public void generateClients() {
-        while (running) {
-            try {
-                long delay = timeGenerationStrategy.getNextGenerationDelay();
-                Thread.sleep(delay);
-                Client newClient =  getClient();
-                //throw Event
-            } catch (InterruptedException e) {
-                running = false;
-                break;
-            }
-        }
-    }
+    private void generateClient() {
+        var moveSystem = TicketSystem.getInstance().getClientMoveSystem();
+        var paydecks = TicketSystem.getInstance().getPayDeckSystem().getPayDecks();
+        var entries = TicketSystem.getInstance().getRoomMap().getEntries();
+        Position startPosition = entries.get(new Random().nextInt(0, entries.size()));
+        Client client = new Client(0,
+                ticketsGenerationStrategy.getTickets(),
+                startPosition,
+                privilegeGenerator.getPrivilege());
 
-    public Client getClient() {
-        return null;
+        scheduler.schedule(this::generateClient,
+                timeGenerationStrategy.getNextGenerationDelay(),
+                TimeUnit.SECONDS);
     };
 
-    public void start() {
-        if (currentThread == null || !currentThread.isAlive()) {
-            running = true;
-            currentThread = new Thread(this::generateClients);
-            currentThread.start();
-        }
+    public void startGenerateClients() {
+        scheduler = new ScheduledThreadPoolExecutor(1);
+
+        scheduler.schedule(this::generateClient,
+                timeGenerationStrategy.getNextGenerationDelay(),
+                TimeUnit.SECONDS);
     }
 
-    public void stop() {
-        running = false;
-        if(currentThread != null) {
-            currentThread.interrupt();
-        }
+    public void stopGenerateClients() {
+        scheduler.shutdownNow();
     }
 }
