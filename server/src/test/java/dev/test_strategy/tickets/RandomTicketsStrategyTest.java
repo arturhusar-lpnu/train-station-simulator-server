63f34a0b7fc57b9b9a0fc55a55f80262f6b5170e
kaflovskyi-roman <kiraqeen2004@gmail.com>
2024-11-23 14:20:09 +0200

    [test] tested all strategies

---
+++ server/src/test/java/dev/test_strategy/tickets/RandomTicketsStrategyTest.java
@@ -0,0 +1,57 @@
+package dev.test_strategy.tickets;
+
+import exceptions.InvalidArgumentException;
+import org.junit.jupiter.api.Test;
+import strategy.tickets.RandomTicketsStrategy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RandomTicketsStrategyTest {
+
+    @Test
+    public void testConstructorWithInvalidArgs() {
+        List<Integer> invalidArgs1 = Arrays.asList(5);
+        List<Integer> invalidArgs2 = null;
+        List<Integer> invalidArgs3 = Arrays.asList(5, 6, 7, 8);
+        List<Integer> invalidArgs4 = new ArrayList<>();
+        assertThrows(InvalidArgumentException.class, () -> new RandomTicketsStrategy(invalidArgs1));
+        assertThrows(InvalidArgumentException.class, () -> new RandomTicketsStrategy(invalidArgs2));
+        assertThrows(InvalidArgumentException.class, () -> new RandomTicketsStrategy(invalidArgs3));
+        assertThrows(InvalidArgumentException.class, () -> new RandomTicketsStrategy(invalidArgs4));
+    }
+
+    @Test
+    public void testConstructorWithValidArgs() throws InvalidArgumentException {
+        List<Integer> args = Arrays.asList(3, 8);
+        RandomTicketsStrategy strategy = new RandomTicketsStrategy(args);
+        assertNotNull(strategy);
+    }
+
+    @Test
+    public void testGetTickets() throws InvalidArgumentException {
+        List<Integer> args = Arrays.asList(5, 10);
+        RandomTicketsStrategy strategy = new RandomTicketsStrategy(args);
+
+        int testCount = 1000;
+        for (int i = 0; i < testCount; i++) {
+            int tickets = strategy.getTickets();
+            assertTrue(tickets >= 5 && tickets <= 10, "Tickets count should be between 5 and 10");
+        }
+    }
+
+    @Test
+    public void testGetTicketsWithReversedArgs() throws InvalidArgumentException {
+        List<Integer> args = Arrays.asList(10, 5);
+        RandomTicketsStrategy strategy = new RandomTicketsStrategy(args);
+
+        int testCount = 1000;
+        for (int i = 0; i < testCount; i++) {
+            int tickets = strategy.getTickets();
+            assertTrue(tickets >= 5 && tickets <= 10, "Tickets count should be between 5 and 10");
+        }
+    }
+}
---
+++ server/src/test/java/dev/test_strategy/time/FixedTimeStrategyTest.java
@@ -0,0 +1,40 @@
+package dev.test_strategy.time;
+
+import exceptions.InvalidArgumentException;
+import org.junit.jupiter.api.Test;
+import strategy.time.FixedTimeStrategy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FixedTimeStrategyTest {
+
+    @Test
+    public void testConstructorWithInvalidArgs() {
+        List<Long> invalidArgs1 = Arrays.asList(5L, 10L);
+        List<Long> invalidArgs2 = null;
+        List<Long> invalidArgs3 = new ArrayList<>();
+        assertThrows(InvalidArgumentException.class, () -> new FixedTimeStrategy(invalidArgs1));
+        assertThrows(InvalidArgumentException.class, () -> new FixedTimeStrategy(invalidArgs2));
+        assertThrows(InvalidArgumentException.class, () -> new FixedTimeStrategy(invalidArgs3));
+    }
+
+    @Test
+    public void testConstructorWithValidArgs() throws InvalidArgumentException {
+        List<Long> args = Arrays.asList(7L); // Один аргумент
+        FixedTimeStrategy strategy = new FixedTimeStrategy(args);
+        assertNotNull(strategy);
+    }
+
+    @Test
+    public void testGetNextGenerationDelay() throws InvalidArgumentException {
+        List<Long> args = Arrays.asList(5L);
+        FixedTimeStrategy strategy = new FixedTimeStrategy(args);
+
+        long tickets = strategy.getNextGenerationDelay();
+        assertEquals(5, tickets, "Tickets count should be 5");
+    }
+}
